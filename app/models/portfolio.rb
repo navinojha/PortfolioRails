e775e21d78df1438808c53bb0deb9fef1f025b73
Navin Ojha <navinojha1996@gmail.com>
2021-11-20 00:31:51 +0530

    Added set defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,20 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body
 
-    scope :portfolio_items, -> { where(:subtitle => "React Js")}
+    # Also can implement Custom Scope using class method
+    def self.react?
+        where(subtitle: "React Js")
+    end
+
+    scope :portfolio_items, -> { where(:subtitle => "React Js")} # 2 way of creating custom scope
     # Ex:- scope :active, -> {where(:active => true)}
+
+    after_initialize :set_defaults
+
+    def set_defaults
+      self.main_image ||= "http://placehold.it/600x400"
+      self.thumb_image ||=  "http://placehold.it/350x200"
+    end
+
+
 end
