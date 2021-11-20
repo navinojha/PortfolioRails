853bc8866ce2f9ff7aa63364d1d5d62406d88675
Navin Ojha <navinojha1996@gmail.com>
2021-11-20 22:46:59 +0530

    Refactored defaults using concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body
 
     # Also can implement Custom Scope using class method
@@ -6,15 +7,14 @@ class Portfolio < ApplicationRecord
         where(subtitle: "React Js")
     end
 
-    scope :portfolio_items, -> { where(:subtitle => "React Js")} # 2 way of creating custom scope
+    scope :portfolio_items, -> { where(:subtitle => "Ruby on Rails")} # 2 way of creating custom scope
     # Ex:- scope :active, -> {where(:active => true)}
 
     after_initialize :set_defaults
 
     def set_defaults
-      self.main_image ||= "http://placehold.it/600x400"
-      self.thumb_image ||=  "http://placehold.it/350x200"
+      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+      self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
-
 
 end
