4d7207f70daa345ee5a93a67a89ec8712125def8
Navin Ojha <navinojha1996@gmail.com>
2021-11-21 00:32:47 +0530

    Added nested attributes for technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
+    accepts_nested_attributes_for :technologies, 
+                                   reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
     include Placeholder
     validates_presence_of :title, :body
 
